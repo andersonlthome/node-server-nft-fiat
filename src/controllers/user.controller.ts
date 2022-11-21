26c5d9f1990cdcb4a44014695f7a3c098308074f
ALT <andersonlottermann@gmail.com>
2022-11-21 00:09:49 -0300

    send scope to front

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import { UserService } from "../services/user.service";
 import {
   Controller,
   Post,
@@ -5,6 +6,7 @@ import {
   SuccessResponse,
   Security,
   Tags,
+  Request,
 } from "tsoa";
 
 @Route("user")
@@ -14,9 +16,10 @@ export class UsersController extends Controller {
   @SuccessResponse("200")
   @Security("jwks", ["user"])
   @Post("verify")
-  public async verify(): Promise<any> {
+  public async verify(@Request() req: any): Promise<any> {
+    const userW3A = await new UserService().findOne(req.verifierId);
     // if pass per authentification return ok
-    return { message: "ok" };
+    return Promise.resolve({ ...userW3A });
   }
 
 }
